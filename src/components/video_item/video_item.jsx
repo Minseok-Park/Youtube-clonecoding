7e81266ca8bcd5782dbe011f1e2781d5366400a7
minseok-Park <pms0905@naver.com>
2021-08-27 12:47:45 +0900

    유튜브 목록 디자인 작업

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video: { snippet } }) => {
   return (
-    <div className="video_item">
-      <img
-        className="video_thumbnail"
-        src={video.snippet.thumbnails.medium.url}
-        alt=""
-      />
-      <div className="video_text">
-        <strong className="video_title">{video.snippet.title}</strong>
-        <span className="video_channel">{video.snippet.channelTitle}</span>
+    <li className={styles.container}>
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="썸네일"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
+    </li>
   );
 };
 
