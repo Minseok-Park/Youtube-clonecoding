14f8211043cb231e8a369b052c4772f7d0475279
minseok-Park <pms0905@naver.com>
2021-08-27 15:05:05 +0900

    youtube service 클래스 구현

--- src/app.jsx
+++ src/app.jsx
@@ -3,49 +3,20 @@ import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
 
-const App = () => {
-  const API_KEY = "AIzaSyDtHaEDsyl-2LEN83CsPcViiAyk_vjVnmM";
-  let query = `bts`;
-
+const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
 
   const search = (query) => {
-    const searchOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=26&q=${query}&type=video&key=${API_KEY}`,
-      searchOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube //
+      .search(query)
+      .then((videos) => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=${API_KEY}`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((data) => setVideos(data.items))
-      .catch((error) => console.log("error", error));
-  }, [videos]);
-
-  const handleSearch = (text) => {
-    query = text;
-    return query;
-  };
+    youtube //
+      .mostPopular()
+      .then((videos) => setVideos(videos));
+  }, []);
 
   return (
     <div className={styles.app}>
---
+++ src/service/youtube.js
@@ -0,0 +1,37 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+  }
+
+  async mostPopular() {
+    try {
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=${this.key}`,
+        this.getRequestOptions
+      );
+      const result = await response.json();
+      return result.items;
+    } catch (error) {
+      return console.log("error", error);
+    }
+  }
+
+  async search(query) {
+    try {
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=26&q=${query}&type=video&key=${this.key}`,
+        this.getRequestOptions
+      );
+      const result = await response.json();
+      return result.items.map((item) => ({ ...item, id: item.id.videoId }));
+    } catch (error) {
+      return console.log("error", error);
+    }
+  }
+}
+
+export default Youtube;
