0c9babebf1c9fd42dc84c7895a2fbb6fe698491e
minseok-Park <pms0905@naver.com>
2021-08-27 17:23:02 +0900

    비디오 상세화면 & 목록 리스트 구현

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -8,29 +8,21 @@ class Youtube {
   }
 
   async mostPopular() {
-    try {
-      const response = await fetch(
-        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&key=${this.key}`,
-        this.getRequestOptions
-      );
-      const result = await response.json();
-      return result.items;
-    } catch (error) {
-      return console.log("error", error);
-    }
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+      this.getRequestOptions
+    );
+    const result = await response.json();
+    return result.items;
   }
 
   async search(query) {
-    try {
-      const response = await fetch(
-        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=26&q=${query}&type=video&key=${this.key}`,
-        this.getRequestOptions
-      );
-      const result = await response.json();
-      return result.items.map((item) => ({ ...item, id: item.id.videoId }));
-    } catch (error) {
-      return console.log("error", error);
-    }
+    const response = await fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+      this.getRequestOptions
+    );
+    const result = await response.json();
+    return result.items.map((item) => ({ ...item, id: item.id.videoId }));
   }
 }
 
